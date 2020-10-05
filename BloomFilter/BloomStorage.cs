fb45b8bc33dd9c71781ea618ce684db5a6a4d254
mikah <mikah@mltd.com>
2020-10-05 15:27:04 -0700

    Add project files.

---
+++ BloomFilter/BloomStorage.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace BloomFilter
+{
+    public class BloomStorage<T>
+    {
+        public List<Func<T, int>> HashFunctions { get; private set; }
+        public int arraySize { get; private set; }
+        public bool[] storage { get; private set; }
+
+        private int DefaultHash(T value)
+        {
+            return value.GetHashCode();
+        }
+
+        public BloomStorage(int arraySize)
+        {
+            HashFunctions = new List<Func<T, int>>();
+            HashFunctions.Add(DefaultHash);
+            this.arraySize = arraySize;
+            storage = new bool[arraySize];
+        }
+
+        public BloomStorage(int arraySize, Func<T, int> HashFunction)
+        {
+            HashFunctions = new List<Func<T, int>>();
+            HashFunctions.Add(HashFunction);
+            this.arraySize = arraySize;
+            storage = new bool[arraySize];
+        }
+
+        public bool AddHash(Func<T, int> newHash)
+        {
+            bool result = true;
+            if(HashFunctions.Contains(newHash))
+            {
+                result = false;
+            }
+            HashFunctions.Add(newHash);
+            return result;
+        }
+
+        public bool RemoveHash(Func<T, int> targetHash)
+        {
+            return HashFunctions.Remove(targetHash);
+        }
+
+        public void Insert(T value)
+        {
+            foreach(Func<T, int> hashFunction in HashFunctions)
+            {
+                storage[Math.Abs(hashFunction(value)) % arraySize] = true;
+            }
+        }
+
+        public bool PossiblyInside(T value)
+        {
+            foreach(Func<T, int> hashFunction in HashFunctions)
+            {
+                if(!storage[Math.Abs(hashFunction(value)) % arraySize])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ BloomFilter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace BloomFilter
+{
+    class Program
+    {
+        static int Hash1(string value)
+        {
+            return (value, "test").GetHashCode();
+        }
+
+        static int Hash2(string value)
+        {
+            return (value, 31).GetHashCode() ^ Hash1(value);
+        }
+
+        static void Main(string[] args)
+        {
+            BloomStorage<string> bloomFilter = new BloomStorage<string>(50);
+            bloomFilter.AddHash(Hash1);
+            bloomFilter.AddHash(Hash2);
+
+
+            bloomFilter.Insert("int");
+            bloomFilter.Insert("string");
+            bloomFilter.Insert("bool");
+            bloomFilter.Insert("array");
+            bloomFilter.Insert("class");
+            bloomFilter.Insert("List");
+            bloomFilter.Insert("Queue");
+            bloomFilter.Insert("Stack");
+            bloomFilter.Insert("Interface");
+            bloomFilter.Insert("HI");
+            bloomFilter.Insert("student");
+            bloomFilter.Insert("teacher");
+            bloomFilter.Insert("computer");
+            bloomFilter.Insert("mouse");
+            bloomFilter.Insert("mice");
+            bloomFilter.Insert("moose");
+            bloomFilter.Insert("house");
+            bloomFilter.Insert("goose");
+            bloomFilter.Insert("geese");
+
+            bool result1 = bloomFilter.PossiblyInside("geese");
+            bool result2 = bloomFilter.PossiblyInside("Queue");
+            bool result3 = bloomFilter.PossiblyInside("queue");
+
+            bool result4 = bloomFilter.PossiblyInside("zoo");
+            bool result5 = bloomFilter.PossiblyInside("cow");
+            bool result6 = bloomFilter.PossiblyInside("eat");
+            bool result7 = bloomFilter.PossiblyInside("studious");
+            bool result8 = bloomFilter.PossiblyInside("programming");
+        }
+    }
+}
